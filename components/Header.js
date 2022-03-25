40b88876e4eb3d8d64b24c5025e56728d5e4a16f
Rajan Khade <chiragkhade@gmail.com>
2022-03-25 21:16:36 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -15,13 +15,13 @@ export default function Header({ name }) {
         </Link>
       </p>
       <form onSubmit={onSubmit} netlify>
-        <label for="fname">First name:</label><br>
-        <input type="text" id="fname" name="fname" value="John"><br>
+        <label for="fname">First name:</label><br />
+        <input type="text" id="fname" name="fname" value="John" /><br />
           
-        <label for="lname">Last name:</label><br>
-        <input type="text" id="lname" name="lname" value="Doe"><br><br>
+        <label for="lname">Last name:</label><br />
+        <input type="text" id="lname" name="lname" value="Doe" /><br /><br />
           
-        <input type="submit" value="Submit">
+        <input type="submit" value="Submit" />
       </form> 
     </header>
   );
